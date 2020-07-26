7b8b263191f358291143aebf53f73df92960bb8b
jordanrob709 <jordanrob709@gmail.com>
2020-07-27 01:50:46 +0300

    added validations and password hashes

--- xbackend/server.js
+++ xbackend/server.js
@@ -3,16 +3,16 @@ const app = express()
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 const userURLS = require('./api/auth')
-const productsURLS = require('./api/products')
+//const productsURLS = require('./api/products')
 
 dotenv.config()
 
-mongoose.connect(process.env.DB_ACCESS, () => {
+mongoose.connect(process.env.DB_ACCESS, { useNewUrlParser: true }, () => {
     console.log('DB CONNECTED!')
 })
 app.use(express.json())
 app.use('/user', userURLS)
-app.use('/products', productsURLS)
+//Qapp.use('/products', productsURLS)
 
 
 app.listen(7000, ()=> {
