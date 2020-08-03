adc626b064aba3bff92c73f58bbcd83c2c81861c
jordanrob709 <jordanrob709@gmail.com>
2020-08-04 02:53:43 +0300

    added content for homepage

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import {Link} from 'react-router-dom'
 
 class Nav extends Component {
     render(){
@@ -6,20 +7,20 @@ class Nav extends Component {
             <div>
                 <nav  className="navbar navbar-expand-lg navbar-light p-3 mb-5 bg-#2c3e50 rounded" >
                     <div className="container" >
-                        <a className="navbar-brand" href="#" style={{ color:"#00B0FF", fontWeight:'bold' }}>Xstore</a>
+                        <Link to='/' className="navbar-brand" href="#" style={{ color:"#00B0FF", fontWeight:'bold' }}>Xstore</Link>
                         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon"></span>
                         </button>
                         <div className="collapse navbar-collapse" id="navbarNav" style={{ marginLeft:"800px" }} >
                             <ul className="navbar-nav" style={{  }}>
                                 <li class="nav-item active" >
-                                    <a class="nav-link" href="#" style={{ color:"#D3D3D3" }}>Home <span class="sr-only">(current)</span></a>
+                                    <Link to='/' class="nav-link" href="#" style={{ color:"#D3D3D3" }}>Home <span class="sr-only">(current)</span></Link>
                                 </li>
                                 <li class="nav-item">
-                                    <a class="nav-link" href="#" style={{ color:"#D3D3D3" }}>Features</a>
+                                    <Link to='/add-product' class="nav-link" href="#" style={{ color:"#D3D3D3" }}>Add-product</Link>
                                 </li>
                                 <li class="nav-item">
-                                    <a class="nav-link" href="#" style={{ color:"#D3D3D3" }}>Pricing</a>
+                                    <Link to='/products' class="nav-link" href="#" style={{ color:"#D3D3D3" }}>Your-Products</Link>
                                 </li>
                             </ul>
                         </div>
