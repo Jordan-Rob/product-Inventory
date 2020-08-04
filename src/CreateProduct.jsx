5553efec9233e6ca32a759fc09e92391d298dad9
jordanrob709 <jordanrob709@gmail.com>
2020-08-04 03:45:40 +0300

    added addProduct content

--- src/CreateProduct.jsx
+++ src/CreateProduct.jsx
@@ -6,8 +6,29 @@ class AddProduct extends Component {
         return ( 
             <div>
                 <div className="skewed"></div>
-                <div className='container'>
-                    <p> Add Product</p>
+                <div className='container' style={{  paddingLeft:'300px', marginTop:'160px', marginBottom:'160px' }}>
+                    <div className="card shadow " style={{ width:'520px', height:'600px' , borderRadius:'15px' , border:'none'}}>
+                    <form action="" method="post" style={{ width:'380px', textAlign:'center', marginLeft:'70px', paddingTop:'90px'}}>
+                        <h3 style={{  paddingBottom:'30px' }}>Add product</h3>
+                        <div className="form-group">
+                            <label htmlFor="name">Name</label>
+                            <input type="text" className="form-control" id='name'/>
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="name">Name</label>
+                            <input type="text" className="form-control" id='name'/>
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="name">Name</label>
+                            <input type="text" className="form-control" id='name'/>
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="name">Name</label>
+                            <input type="text" className="form-control" id='name'/>
+                        </div>
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </form>
+                    </div>
                 </div>
             </div>
          );
