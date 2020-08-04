97b80dd4f31e773862a700d5550530d628baf91a
jordanrob709 <jordanrob709@gmail.com>
2020-08-04 03:11:37 +0300

    added nav links & routes

---
+++ src/CreateProduct.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class AddProduct extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div>
+                <div className="skewed"></div>
+                <div className='container'>
+                    <p> Add Product</p>
+                </div>
+            </div>
+         );
+    }
+}
+ 
+export default AddProduct;
---
+++ src/Products.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class Products extends Component {
+    render(){
+        return(
+        
+            <div>
+                <div className="skewed"></div>
+                <div className='container'>
+                    <p>Products</p>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Products
