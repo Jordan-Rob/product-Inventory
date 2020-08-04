a6652884fbbc7f8d861808b829dca6308b2d5414
jordanrob709 <jordanrob709@gmail.com>
2020-08-04 11:51:00 +0300

    added addProduct content

--- src/CreateProduct.jsx
+++ src/CreateProduct.jsx
@@ -8,23 +8,23 @@ class AddProduct extends Component {
                 <div className="skewed"></div>
                 <div className='container' style={{  paddingLeft:'300px', marginTop:'160px', marginBottom:'160px' }}>
                     <div className="card shadow " style={{ width:'520px', height:'600px' , borderRadius:'15px' , border:'none'}}>
-                    <form action="" method="post" style={{ width:'380px', textAlign:'center', marginLeft:'70px', paddingTop:'90px'}}>
+                    <form action="" method="post" style={{ width:'380px', textAlign:'', marginLeft:'70px', paddingTop:'90px'}}>
                         <h3 style={{  paddingBottom:'30px' }}>Add product</h3>
                         <div className="form-group">
                             <label htmlFor="name">Name</label>
                             <input type="text" className="form-control" id='name'/>
                         </div>
                         <div className="form-group">
-                            <label htmlFor="name">Name</label>
-                            <input type="text" className="form-control" id='name'/>
+                            <label htmlFor="img">Image</label>
+                            <input type="file" class="form-control-file" id='img'/>
                         </div>
                         <div className="form-group">
-                            <label htmlFor="name">Name</label>
-                            <input type="text" className="form-control" id='name'/>
+                            <label htmlFor="price">Price</label>
+                            <input type="text" className="form-control" id='price'/>
                         </div>
-                        <div className="form-group">
-                            <label htmlFor="name">Name</label>
-                            <input type="text" className="form-control" id='name'/>
+                        <div className="form-group form-check">
+                            <input type="checkbox" class="form-check-input" id="avail"/>
+                            <label class="form-check-label" htmlFor="avail">Available</label>
                         </div>
                         <button type="submit" class="btn btn-primary">Submit</button>
                     </form>
